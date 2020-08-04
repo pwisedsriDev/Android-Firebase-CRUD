3caf98af6b128f2b906d576c2a88461f58bca354
pwisedsri_dev <wisedsri2@gmail.com>
2020-08-04 06:50:54 +0100

    Update SetValueByID.java

--- SetValueByID.java
+++ SetValueByID.java
@@ -16,3 +16,23 @@ ref.child("users").child(account.getId()).setValue(user).addOnSuccessListener(ne
         Toast.makeText(UserInfoUpdate.this, "User Info Update Failed!", Toast.LENGTH_SHORT).show();
     }
 });
+
+
+DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+// DatabaseReference ref=FirebaseDatabase.getInstance().getReference().child("Users").child("Field 1");
+Map<String, Object> updates = new HashMap<String,Object>();
+updates.put("key", gender); // Values 
+
+ref.child("users").child(account.getId()).updateChildren(updates).addOnSuccessListener(new OnSuccessListener<Void>() {
+    @Override
+    public void onSuccess(Void aVoid) {
+        Toast.makeText(getApplicationContext(), "User Info Update Success!", Toast.LENGTH_SHORT).show();
+    }
+}).addOnFailureListener(new OnFailureListener() {
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        // Write failed
+        Toast.makeText(getApplicationContext(), "User Info Update Failed!", Toast.LENGTH_SHORT).show();
+    }
+});
